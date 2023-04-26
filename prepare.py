acf7327d3b642bab9bd2ded7f666214997ceaea0
Nikita Ushanov <64222243+NikitaUshanov@users.noreply.github.com>
2023-04-26 21:49:35 +0300

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -4,27 +4,20 @@ from sklearn import preprocessing
 df = pd.read_csv('C:/Users/n.ushanov/Downloads/responce_train.csv')
 df = df.drop(["client_id"], axis=1)
 
-def df_nulls():
-    print(df.isnull().sum())
-
-def text_to_int(data):
+def text_to_int(data, columns):
     le = preprocessing.LabelEncoder()
-    columns = ["education", "marital-status", "industry", "title", "tp-state", "tp-foreign", "job-type", "family-income", "registration-region", "fact-region", "postal-region", "last-loan-region", "region"]
-    set_del_col = set(columns)
     for col in columns:
         data[col] = le.fit_transform(data[col])
-        print(le.classes_)
-    return set_del_col
+    return data
 
 def clean(data):
-    cols = list(set(data.keys())-set_del_col)
-    print(cols)
-    # можно оставить пустые ячейки
+    cols = list(data.keys())
     for col in cols:
         data[col].fillna(data[col].median(), inplace=True)
-
     return data
 
 
-set_del_col = text_to_int(df)
-clean(df)
+columns = ["education", "marital-status", "industry", "title", "tp-state", "tp-foreign", "job-type", "family-income", "registration-region", "fact-region", "postal-region", "last-loan-region", "region"]
+
+df = text_to_int(df, columns)
+df = clean(df)
